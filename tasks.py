aab0cf287811087992d036f980bf2b41c1ea3617
Miguel Martin Hidalgo <mfhidalgo18@hotmail.com>
2025-10-31 16:22:45 +0100

    Assignment 1 - finished version

--- tasks.py
+++ tasks.py
@@ -1,3 +1,7 @@
+for name in dir():
+    if not name.startswith(""):
+        del globals()[name]
+
 import numpy
 
 # Follow the tasks below to practice basic Python concepts.
@@ -13,11 +17,18 @@ import numpy
 # Your code here:
 # -----------------------------------------------
 
-def step
+def step(x):
+  one_numeric = x
+  if one_numeric > 0:
+    return 1
+  if one_numeric < 0 or one_numeric == 0:
+    return -1
 
+step(5)
+step(-3)
+step(0)
 
 # -----------------------------------------------
-
 
 # Task 2:
 # Instructions:
@@ -25,14 +36,17 @@ def step
 #The function should return a numpy array of the same length, with all elements smaller than the cutoff being set to cutoff).
 #The name of the function should be ReLu
 
-
 # Your code here:
 # -----------------------------------------------
-def ReLu
 
+def ReLu(a: numpy.array, cutoff = int(0)):
+  return numpy.maximum(a, cutoff)
 
+ReLu([-5, 0, 3, -2, 4])
+ReLu([-5, 0, 3, -2, 4], cutoff = 2)
+ReLu([])
+  
 # -----------------------------------------------
-
 
 # Task 3:
 # Instructions:
@@ -44,7 +58,21 @@ def ReLu
 # Your code here:
 # -----------------------------------------------
 
-def neural_net_layer
+def neural_net_layer(a: numpy.array, b: numpy.array):
+  mult = a @ b
+  return ReLu(mult)
+    
+inputs1 = numpy.array([[1, 2], [3, 4]])
+weights1 = numpy.array([1, -1])
 
+inputs2 = numpy.array([[2, 3], [1, 1]])
+weights2 = numpy.array([1, 2])
+
+inputs3 = numpy.array([[1, -1], [-2, 3]])
+weights3 = numpy.array([-2, 1])
+
+neural_net_layer(inputs1, weights1)
+neural_net_layer(inputs2, weights2)
+neural_net_layer(inputs3, weights3)
 
 # ------------------------------------------
